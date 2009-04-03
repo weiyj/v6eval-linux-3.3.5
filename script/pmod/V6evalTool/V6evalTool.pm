4018fde83629d36d91f16c10ed9d7362ff968c1f
Wei Yongjun <weiyj.lk@gmail.com>
2009-04-03 02:51:37 -0400

    script: new function vLogTitle for V6evalTool.pm
    This patch add new function vLogTitle to V6evalTool.pm,
    Unlike vLogHTML() function, vLogTitle will close the
    teble after vLogTitle() write HTML to log file.
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>

--- script/pmod/V6evalTool/V6evalTool.pm
+++ script/pmod/V6evalTool/V6evalTool.pm
@@ -64,6 +64,7 @@ use File::Basename;
 	vClear
 	vLog
 	vLogHTML
+	vLogTitle
 	vErrmsg
 	vRoundoff
 	vRemote
@@ -930,6 +931,22 @@ vLogHTML($)
 	};
 	prLogHTML($message);
 	prOut "$message" if $VLog == 0;
+}
+
+
+
+########################################################################
+sub
+vLogTitle($)
+{
+	my ($message,	# message to be logged
+    ) = @_;
+	prLog("</TD></TR>\n") if($vLogStat==$vLogStatOpenRow);
+	prLog("<TR><TD><BR></TD><TD>");
+	prLogHTML($message);
+	prOut "$message" if $VLog == 0;
+	prLog("</TD></TR>\n");
+	$vLogStat=$vLogStatCloseRow;
 }
 
 
